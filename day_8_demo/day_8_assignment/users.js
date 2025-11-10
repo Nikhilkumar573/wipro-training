4a9d29719a05d351bc5857493f788456a7e3d999
Nikhilkumar573 <125425308+Nikhilkumar573@users.noreply.github.com>
2025-11-10 23:41:11 +0530

    Update users.js

--- day_8_demo/day_8_assignment/users.js
+++ day_8_demo/day_8_assignment/users.js
@@ -1,20 +1,20 @@
 db.Users.insertMany([
   {
    
-    "name": "vishu",
-    "email": "vishu@example.com",
+    "name": "nikhil",
+    "email": "nikhil@example.com",
     "joinDate": ISODate("2023-06-15T00:00:00Z")
   },
   {
  
-    "name": "yash",
-    "email": "yash@example.com",
+    "name": "suman",
+    "email": "suman@example.com",
     "joinDate": ISODate("2024-01-10T00:00:00Z")
   },
   {
 
-    "name": "harsh",
-    "email": "harsh@example.com",
+    "name": "shaswat",
+    "email": "shaswat@example.com",
     "joinDate": ISODate("2024-09-05T00:00:00Z")
   }
 ]
