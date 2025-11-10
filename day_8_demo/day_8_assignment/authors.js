284fa7217f934e757c3ce7ee38eba05fcd8b7312
Nikhilkumar573 <125425308+Nikhilkumar573@users.noreply.github.com>
2025-11-10 23:39:48 +0530

    Update authors.js

--- day_8_demo/day_8_assignment/authors.js
+++ day_8_demo/day_8_assignment/authors.js
@@ -1,5 +1,5 @@
 db.Authors.insertMany([
-  { name: "vishwas", nationality: "indian", birthYear: 2002 },
-  { name: "jony", nationality: "america", birthYear: 2000 },
-  { name: "don", nationality: "russia", birthYear: 1995 }
+  { name: "nikhil", nationality: "indian", birthYear: 2002 },
+  { name: "shaswat", nationality: "america", birthYear: 2000 },
+  { name: "suman", nationality: "russia", birthYear: 1995 }
 ]);
